ab9925a3c069b56e682ed4acc97477dce9e64646
Vítor <emailreservadovitor@gmail.com>
2015-08-10 15:39:27 -0300

    2 new methods for question 4
    TODO: method for 4.4, a main to run it

--- src/Simulator.java
+++ src/Simulator.java
@@ -92,6 +92,157 @@ public class Simulator {
         return meanNumberOfClients;
     }
 
+    public double meanTotalExitTime() { // For 4.1 and 4.3
+        double area = 0;
+        double lastNumberOfClients = 0;
+        double lastEventTime = 0;
+        double sumTimes = 0;
+        double thisExistsToUseDistribution2Times =0;
+        double numberOfExits = 0;
+
+        while (!events.isEmpty()) {
+            Event currentEvent = events.remove(0);
+
+            if(currentEvent.getTime()>TIME_LIMIT) {
+                break;
+            }
+
+            switch (currentEvent.getType()) {
+                case entry:
+                    numberOfClients++;
+//                    System.out.println("Entrou, N=" + numberOfClients);
+
+                    double newEntryTime = currentEvent.getTime() + entryDistribution.nextNumber();
+                    Event newEntryEvent = new Event(newEntryTime, Event.EventType.entry);
+                    addEvent(newEntryEvent);
+
+                    if(numberOfClients == 1) {
+
+                        double newExitTime = currentEvent.getTime() + exitDistribution.nextNumber();
+                        Event newExitEvent = new Event(newExitTime, Event.EventType.exit);
+                        addEvent(newExitEvent);
+
+                    }
+
+                    break;
+
+                case exit:
+
+                    thisExistsToUseDistribution2Times = exitDistribution.nextNumber();
+                    sumTimes += thisExistsToUseDistribution2Times;
+                    numberOfExits++;
+
+                    if(!(random.nextDouble() < reentryProbability)) {
+                        numberOfClients--;
+                    }
+
+                    if(numberOfClients>0) {
+
+                        double newExitTime = currentEvent.getTime() + thisExistsToUseDistribution2Times;
+                        Event newExitEvent = new Event(newExitTime, Event.EventType.exit);
+                        addEvent(newExitEvent);
+
+                    }
+                    break;
+            }
+
+            area += lastNumberOfClients*(currentEvent.getTime()-lastEventTime);
+            lastEventTime = currentEvent.getTime();
+            lastNumberOfClients = numberOfClients;
+        }
+
+        double meanNumberOfClients = area/lastEventTime;
+//        System.out.println("The simulation ended after " + lastEventTime);
+//        System.out.println("The average numbers of clients was " + meanNumberOfClients);
+        return sumTimes/numberOfExits;
+    }
+
+    public double meanExogenExitTimes() {// For 4.2
+        double area = 0;
+        double lastNumberOfClients = 0;
+        double lastEventTime = 0;
+        double sumTimes = 0;
+        double thisExistsToUseDistribution2Times =0;
+        double numberOfExits = 0;
+
+        while (!events.isEmpty()) {
+            Event currentEvent = events.remove(0);
+
+            if(currentEvent.getTime()>TIME_LIMIT) {
+                break;
+            }
+
+            switch (currentEvent.getType()) {
+                case entry:
+                    numberOfClients++;
+//                    System.out.println("Entrou, N=" + numberOfClients);
+
+                    double newEntryTime = currentEvent.getTime() + entryDistribution.nextNumber();
+                    Event newEntryEvent = new Event(newEntryTime, Event.EventType.entry);
+                    addEvent(newEntryEvent);
+
+                    if(numberOfClients == 1) {
+
+                        double newExitTime = currentEvent.getTime() + exitDistribution.nextNumber();
+                        Event newExitEvent = new Event(newExitTime, Event.EventType.exit);
+                        addEvent(newExitEvent);
+
+                    }
+
+                    break;
+
+                case exit:
+
+                    if(!(random.nextDouble() < reentryProbability)) {
+                        numberOfClients--;
+
+
+                        if (numberOfClients > 0) {
+
+                            double newExitTime = currentEvent.getTime() + exitDistribution.nextNumber();
+                            Event newExitEvent = new Event(newExitTime, Event.EventType.exit);
+                            addEvent(newExitEvent);
+
+                        }
+
+                    }
+
+                    else {
+
+                        numberOfExits++;
+                        thisExistsToUseDistribution2Times = exitDistribution.nextNumber();
+                        sumTimes += thisExistsToUseDistribution2Times;
+
+                        if (numberOfClients > 0) {
+
+                            double newExitTime = currentEvent.getTime() + exitDistribution.nextNumber();
+                            Event newExitEvent = new Event(newExitTime, Event.EventType.exit);
+                            addEvent(newExitEvent);
+
+                        }
+
+
+
+
+
+                    }
+
+
+                    break;
+            }
+
+            area += lastNumberOfClients*(currentEvent.getTime()-lastEventTime);
+            lastEventTime = currentEvent.getTime();
+            lastNumberOfClients = numberOfClients;
+        }
+
+        double meanNumberOfClients = area/lastEventTime;
+//        System.out.println("The simulation ended after " + lastEventTime);
+//        System.out.println("The average numbers of clients was " + meanNumberOfClients);
+        return sumTimes/numberOfExits;
+    }
+
+
     private void addEvent(Event event) {
         events.add(event);
         Collections.sort(events);
